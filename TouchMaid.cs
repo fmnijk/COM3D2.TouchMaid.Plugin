5dd9184502cd55fbae4e04f5262bc9ae6325c21e
fmnijk <fmnijk@gmail.com>
2021-08-17 00:36:55 +0800

    Update TouchMaid.cs

--- TouchMaid.cs
+++ TouchMaid.cs
@@ -1,71 +1,120 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using BepInEx;
 using BepInEx.Configuration;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using HarmonyLib;
+using System.Collections;
 
+//TODO:使胸部晃動情況可大於100
+//TODO:控制頭部
+//TODO:滑鼠控制胸部
+//TODO:控制頭髮
+//TODO:控制裙子
+/*if (Input.GetMouseButton(0))
+{
+    var a = Input.mousePosition;
+    List<Maid> maids = GameMain.Instance.CharacterMgr.GetStockMaidList();
+    if (maids != null)
+    {
+        foreach (Maid m in maids)
+        {
+            if (m.Visible == true)
+            {
+                Vector3 vector;
+                vector = (Vector3)AccessTools.Field(typeof(jiggleBone), "dynamicPos").GetValue(m.body0.jbMuneL);
+                Debug.Log(vector);
+            }
+        }
+    }
+}*/
 
 namespace TouchMaid
 {
     [BepInPlugin("org.fmnijk.plugins.touchmaid", "COM3D2.TouchMaid.Plugin", "1.0.0.1")]
     public class TouchMaid : BaseUnityPlugin
     {
-        private ConfigEntry<KeyboardShortcut> TouchMaidChest { get; set; }
+        private ConfigEntry<KeyboardShortcut> TouchMuneHotkey { get; set; }
+        private ConfigEntry<KeyboardShortcut> TouchMuneLoopHotkey { get; set; }
 
         public void Awake()
         {
-            TouchMaidChest = Config.Bind("Hotkeys", "TouchMaid Hotkeys", new KeyboardShortcut(KeyCode.Keypad1, KeyCode.LeftShift));
+            TouchMuneHotkey = Config.Bind("Hotkeys", "TouchMuneHotkey", new KeyboardShortcut(KeyCode.D, KeyCode.LeftShift));
+            TouchMuneLoopHotkey = Config.Bind("Hotkeys", "TouchMuneLoopHotkey", new KeyboardShortcut(KeyCode.F, KeyCode.LeftShift));
+        }
+
+        public void TouchHead()
+        {
+            List<Maid> maids = GameMain.Instance.CharacterMgr.GetStockMaidList();
+            if (maids != null)
+            {
+                foreach (Maid m in maids)
+                {
+                    if (m.Visible == true)
+                    {
+                        Vector3 vector;
+
+                        vector = (Vector3)AccessTools.Field(typeof(TBody), "HeadEulerAngle").GetValue(m.body0);
+                        Debug.Log(vector);
+                        vector += Random.insideUnitSphere * 30;
+                        AccessTools.Field(typeof(TBody), "HeadEulerAngle").SetValue(m.body0, vector);
+                    }
+                }
+            }
+        }
+
+        public void TouchMune()
+        {
+            List<Maid> maids = GameMain.Instance.CharacterMgr.GetStockMaidList();
+            if (maids != null)
+            {
+                foreach (Maid m in maids)
+                {
+                    if (m.Visible == true)
+                    {
+                        Vector3 vector;
+
+                        vector = (Vector3)AccessTools.Field(typeof(jiggleBone), "dynamicPos").GetValue(m.body0.jbMuneL);
+                        vector += Random.insideUnitSphere / 2;
+                        AccessTools.Field(typeof(jiggleBone), "dynamicPos").SetValue(m.body0.jbMuneL, vector);
+
+                        vector = (Vector3)AccessTools.Field(typeof(jiggleBone), "dynamicPos").GetValue(m.body0.jbMuneR);
+                        vector += Random.insideUnitSphere / 2;
+                        AccessTools.Field(typeof(jiggleBone), "dynamicPos").SetValue(m.body0.jbMuneR, vector);
+
+                        vector = (Vector3)AccessTools.Field(typeof(jiggleBone), "vel").GetValue(m.body0.jbMuneL);
+                        vector += Random.insideUnitSphere / 10;
+                        AccessTools.Field(typeof(jiggleBone), "vel").SetValue(m.body0.jbMuneL, vector);
+
+                        vector = (Vector3)AccessTools.Field(typeof(jiggleBone), "vel").GetValue(m.body0.jbMuneR);
+                        vector += Random.insideUnitSphere / 10;
+                        AccessTools.Field(typeof(jiggleBone), "vel").SetValue(m.body0.jbMuneR, vector);
+                    }
+                }
+            }
         }
 
         public void Update()
         {
             try
             {
-                if (TouchMaidChest.Value.IsDown())
+                if (TouchMuneHotkey.Value.IsDown())
                 {
-                    List<Maid> maids = GameMain.Instance.CharacterMgr.GetStockMaidList();
-                    if (maids != null)
+                    TouchMune();
+                }
+                if (TouchMuneLoopHotkey.Value.IsDown())
+                {
+                    if (IsInvoking("TouchMune"))
                     {
-                        foreach (Maid m in maids)
-                        {
-                            if (m.Visible == true)
-                            {
-                                Vector3 vector;
-
-                                vector = (Vector3)AccessTools.Field(typeof(jiggleBone), "dynamicPos").GetValue(m.body0.jbMuneL);
-                                vector.x += UnityEngine.Random.Range(-0.3f, 0.3f);
-                                vector.y += UnityEngine.Random.Range(-0.3f, 0.3f);
-                                vector.z += UnityEngine.Random.Range(-0.3f, 0.3f);
-                                AccessTools.Field(typeof(jiggleBone), "dynamicPos").SetValue(m.body0.jbMuneL, vector);
-
-                                vector = (Vector3)AccessTools.Field(typeof(jiggleBone), "dynamicPos").GetValue(m.body0.jbMuneR);
-                                vector.x += UnityEngine.Random.Range(-0.3f, 0.3f);
-                                vector.y += UnityEngine.Random.Range(-0.3f, 0.3f);
-                                vector.z += UnityEngine.Random.Range(-0.3f, 0.3f);
-                                AccessTools.Field(typeof(jiggleBone), "dynamicPos").SetValue(m.body0.jbMuneR, vector);
-                                
-
-                                vector = (Vector3)AccessTools.Field(typeof(jiggleBone), "vel").GetValue(m.body0.jbMuneL);
-                                vector.x += UnityEngine.Random.Range(-0.1f, 0.1f);
-                                vector.y += UnityEngine.Random.Range(-0.1f, 0.1f);
-                                vector.z += UnityEngine.Random.Range(-0.1f, 0.1f);
-                                AccessTools.Field(typeof(jiggleBone), "vel").SetValue(m.body0.jbMuneL, vector);
-
-                                vector = (Vector3)AccessTools.Field(typeof(jiggleBone), "vel").GetValue(m.body0.jbMuneR);
-                                vector.x += UnityEngine.Random.Range(-0.1f, 0.1f);
-                                vector.y += UnityEngine.Random.Range(-0.1f, 0.1f);
-                                vector.z += UnityEngine.Random.Range(-0.1f, 0.1f);
-                                AccessTools.Field(typeof(jiggleBone), "vel").SetValue(m.body0.jbMuneR, vector);
-                            }
-                        }
+                        CancelInvoke("TouchMune");
+                    }
+                    else
+                    {
+                        InvokeRepeating("TouchMune", 0f, 2.5f);
                     }
                 }
             }
-            catch (Exception e)
+            catch (System.Exception e)
             {
                 Debug.Log("TouchMaid Plug-In Error!");
                 Debug.Log(e);
